c100e08c91b0fbb35b37a9fc66a68e4598618d8a
ratul9 <ratuldas.ctg2016@gmail.com>
2022-03-05 21:14:18 +0600

    Add project files.

---
+++ Controllers/TestingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace routing_tut.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TestingController : ControllerBase
+	{
+
+		[HttpGet("/{age:int:minlength(2)}")]
+		public async Task<string> getHello(int age, [FromQuery] String name)
+		{
+			return $"Passed name is : {name ?? "Not Provided"}.Passed age is : {age}";
+		}
+
+		[HttpGet("/includes-anik/{value:anik}")]
+		public async Task<string> getTestingCustom(string? value)
+		{
+			return $"Passed name is : {value ?? "Not Provided"}";
+		}
+	}
+}
---
+++ CustomRoutingConstraints/ContainsAnikConstratint.cs
@@ -0,0 +1,20 @@
+﻿namespace routing_tut.CustomRoutingConstraints
+{
+	public class ContainsAnikConstratint : IRouteConstraint
+	{
+		public bool Match(HttpContext? httpContext, IRouter? route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+		{
+			if(values.TryGetValue(routeKey, out object passedValue))
+			{
+				if(passedValue is string stringObj)
+				{
+					if (stringObj.Contains("anik"))
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+	}
+}
